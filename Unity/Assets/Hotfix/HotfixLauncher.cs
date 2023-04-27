409966805a6f60b1a460337eed906650f59e00c7
AlianBlank <alianblank@outlook.com>
2023-04-27 14:47:59 +0800

    [修改]1. 修改日志打印

--- Unity/Assets/Hotfix/HotfixLauncher.cs
+++ Unity/Assets/Hotfix/HotfixLauncher.cs
@@ -1,5 +1,12 @@
-﻿using Animancer;
+﻿using System.IO;
+using System.Text;
+using Animancer;
+using GameFramework;
+using Hotfix.Message.Proto;
+using ProtoBuf;
+using ProtoBuf.Meta;
 using UnityEngine;
+using UnityGameFramework.Runtime;
 
 namespace Hotfix
 {
@@ -7,9 +14,20 @@ namespace Hotfix
     {
         public static void Main()
         {
-            UnityGameFramework.Runtime.Log.Info("Hello World");
+            UnityGameFramework.Runtime.Log.Info("Hello World hclr");
             GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<AnimancerComponent>();
-            GameApp.Lua.DoString("CS.UnityEngine.Debug.Log('Hello World')");
+            GameApp.Lua.DoString("CS.UnityEngine.Debug.Log('Hello World luw')");
+            MemoryStream memoryStream = new MemoryStream();
+            var userInfo = new CSHeartBeat();
+            userInfo.Timestamp = 11111;
+            RuntimeTypeModel.Default.Serialize(memoryStream, userInfo);
+            var buffer = memoryStream.ToArray();
+
+            using (MemoryStream desMemoryStream = new MemoryStream(buffer))
+            {
+                var deserializedData = RuntimeTypeModel.Default.Deserialize(desMemoryStream, null, typeof(CSHeartBeat));
+                Log.Warning(Utility.Json.ToJson(deserializedData));
+            }
         }
     }
 }
