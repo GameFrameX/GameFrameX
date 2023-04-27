bd4a3e60a919c042a745bcca6614b850056962ec
AlianBlank <alianblank@outlook.com>
2023-04-27 14:49:06 +0800

    [增加]1. 增加Proto文件转换功能

---
+++ Unity/Packages/com.alianblank.protobuff2cs@1.0.0/Editor/Proto2CSEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Proto2CS.Editor
+{
+    internal static class Proto2CSEditor
+    {
+        [MenuItem("Tools/Proto2CS &E")]
+        internal static void AllProto2Cs()
+        {
+            UtilityProto2Cs.Proto2Cs("./../Protobuf", "./Assets/Hotfix/Network/Message/Proto", "Hotfix.Message.Proto");
+            UnityEngine.Debug.Log("导出协议完成");
+            AssetDatabase.Refresh();
+        }
+    }
+}
