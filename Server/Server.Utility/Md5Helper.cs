1a9ef8fa2b7f3a456af3977e3647f16ecbdb9d30
AlianBlank <alianblank@outlook.com>
2023-05-05 15:46:14 +0800

    [优化]1. 代码优化

--- Server/Server.Utility/Md5Helper.cs
+++ Server/Server.Utility/Md5Helper.cs
@@ -4,13 +4,13 @@ namespace Server.Utility
 {
     public static class Md5Helper
     {
-        private static readonly ThreadLocal<MD5> _md5Cache = new(() => MD5.Create());
+        private static readonly ThreadLocal<MD5> Md5Cache = new(MD5.Create);
 
-        public static MD5 MD5Current => _md5Cache.Value;
+        public static MD5 Md5Current => Md5Cache.Value;
 
         public static string Md5(byte[] inputBytes)
         {
-            var hashBytes = MD5Current.ComputeHash(inputBytes);
+            var hashBytes = Md5Current.ComputeHash(inputBytes);
             var hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
             return hash;
         }
