b85224631ae8d5d600c95e78f8eab735a6ad66c1
AlianBlank <alianblank@outlook.com>
2023-07-01 14:11:06 +0800

    [增加]1. 增加[配置文件的处理

--- Unity/Assets/Hotfix/HotfixLauncher.cs
+++ Unity/Assets/Hotfix/HotfixLauncher.cs
@@ -6,11 +6,16 @@ namespace Hotfix
 {
     public static class HotfixLauncher
     {
+        public static string serverIp = "127.0.0.1";
+        public static int serverPort = 8898;
+
         public static void Main()
         {
             Log.Info("Hello World HybridCLR");
             GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<AnimancerComponent>();
             GameApp.Lua.DoString("CS.UnityEngine.Debug.Log('Hello World Lua')");
+
+            LoadConfig();
             // MemoryStream memoryStream = new MemoryStream();
             // var userInfo = new CSHeartBeat();
             // userInfo.Timestamp = 11111111;
@@ -22,6 +27,24 @@ namespace Hotfix
             //     var deserializedData = RuntimeTypeModel.Default.Deserialize(desMemoryStream, null, typeof(CSHeartBeat));
             //     Log.Warning(Utility.Json.ToJson(deserializedData));
             // }
+        }
+        static void LoadConfig()
+        {
+            // var tables = new cfg.Tables(file =>
+            //     JSON.Parse(File.ReadAllText(AssetUtility.GetConfigPath(file, "json"))
+            //     ));
+
+
+            var tables = new cfg.Tables(Loader);
+            var item = tables.TbItem.Get(1);
+            Log.Info(item);
+        }
+
+        private static JSONNode Loader(string file)
+        {
+            var rawFileOperationHandle = GameApp.Asset.LoadRawFileSync(AssetUtility.GetConfigPath(file, ".json"));
+
+            return JSON.Parse(rawFileOperationHandle.GetRawFileText());
         }
     }
 }
