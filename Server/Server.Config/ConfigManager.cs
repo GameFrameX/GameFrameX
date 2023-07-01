46bd6f004b57ed29a46d251adef8f31dbcff2883
AlianBlank <alianblank@outlook.com>
2023-07-01 14:08:44 +0800

    [增加]1. 增加配置文件的处理

---
+++ Server/Server.Config/ConfigManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using cfg;
+
+namespace Server.Config;
+
+public class ConfigManager
+{
+    public static ConfigManager Instance { get; } = new ConfigManager();
+    public Tables Tables { get; private set; }
+
+    public void LoadConfig()
+    {
+        Tables = new cfg.Tables(Loader);
+        Console.WriteLine("== load success ==");
+    }
+
+    private static JsonElement Loader(string file)
+    {
+        var configJson = File.ReadAllBytes($"json/{file}.json");
+        JsonElement jsonElement = JsonDocument.Parse(configJson).RootElement;
+        return jsonElement;
+    }
+}
