5d404f4830ac68e6b73eae43ad1cfc11d355c0a8
AlianBlank <alianblank@outlook.com>
2023-08-17 11:45:23 +0800

    [增加]1. 增加编辑器工具类的快捷锁定Inspector面板功能

---
+++ Unity/Assets/Editor/EditorTools/InspectorLockShortcut.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+public class InspectorLockShortcut : EditorWindow
+{
+    [MenuItem("Window/Toggle Inspector Lock %l")]
+    private static void ToggleInspectorLock()
+    {
+        // 获取Inspector窗口
+        var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+        var inspectorWindow = EditorWindow.GetWindow(inspectorType);
+        // 使用反射调用isLocked属性
+        var isLockedProperty = inspectorType.GetProperty("isLocked");
+        var isLocked = (bool) isLockedProperty.GetValue(inspectorWindow);
+        isLockedProperty.SetValue(inspectorWindow, !isLocked);
+        // PrefabUtility.CreatePrefab( "Assets/Editor/ToggleInspectorLock.prefab", inspectorWindow);
+    }
+}
