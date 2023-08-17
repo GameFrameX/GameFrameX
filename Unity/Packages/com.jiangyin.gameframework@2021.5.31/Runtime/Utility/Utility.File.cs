7a08fe260255b0b7557b188cd552956132cb1cef
AlianBlank <alianblank@outlook.com>
2023-08-17 11:41:20 +0800

    [拆分]1. 拆分工具类

---
+++ Unity/Packages/com.jiangyin.gameframework@2021.5.31/Runtime/Utility/Utility.File.cs
@@ -0,0 +1,31 @@
+﻿namespace GameFramework
+{
+    public static partial class Utility
+    {
+        /// <summary>
+        /// 文件相关的实用函数
+        /// </summary>
+        public static class File
+        {
+            private static readonly string[] UnitList = new[] {"B", "KB", "MB", "GB", "TB", "PB"};
+
+            /// <summary>
+            /// 获取字节大小
+            /// </summary>
+            public static string GetBytesSize(long size)
+            {
+                foreach (var unit in UnitList)
+                {
+                    if (size <= 1024)
+                    {
+                        return size + unit;
+                    }
+
+                    size /= 1024;
+                }
+
+                return size + UnitList[0];
+            }
+        }
+    }
+}
