17d550bdd083ccd82468b4d2dc8e41bc11d8dbb3
AlianBlank <alianblank@outlook.com>
2023-08-17 22:29:13 +0800

    [优化]1. 将Log拆分到单独程序集

---
+++ Server/Server.Log/LoggerHandler.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.LayoutRenderers;
+
+namespace Server.Log;
+
+public static class LoggerHandler
+{
+    static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+    public static bool Start()
+    {
+        try
+        {
+            Console.WriteLine("init NLog config...");
+            LayoutRenderer.Register<NLogConfigurationLayoutRender>("logConfiguration");
+            LogManager.Configuration = new XmlLoggingConfiguration("Configs/app_log.config");
+            LogManager.AutoShutdown = false;
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"启动服务器失败,异常:{e}");
+            return false;
+        }
+    }
+}
