7e2daef328b2d776469f118a1ded7e3dbbb4ec76
AlianBlank <alianblank@outlook.com>
2023-08-24 18:30:10 +0800

    [增加]1. 增加服务器的协议格式化器的不生成的时候的空类调用

---
+++ Server/Server.Proto/PolymorphicRegister.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Server.Proto.Formatter
+{
+    public partial class PolymorphicRegister
+    {
+        public static void Load()
+        {
+        }
+    }
+}
