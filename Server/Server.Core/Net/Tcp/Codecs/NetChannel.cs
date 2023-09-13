957f52aad5bfb0b422be093bf393f8ff8d4e6351
Blank <wangfj11@foxmail.com>
2023-09-14 00:06:00 +0800

    [增加]1. 增加服务器网络频道的心跳对象

--- Server/Server.Core/Net/Tcp/Codecs/NetChannel.cs
+++ Server/Server.Core/Net/Tcp/Codecs/NetChannel.cs
@@ -4,6 +4,53 @@ using Server.Core.Net.Messages;
 
 namespace Server.Core.Net.Tcp.Codecs
 {
+    /// <summary>
+    /// 心跳状态
+    /// </summary>
+    public sealed class HeartBeatStatus
+    {
+        private float m_HeartBeatElapseSeconds;
+        private int m_MissHeartBeatCount;
+
+        public HeartBeatStatus()
+        {
+            m_HeartBeatElapseSeconds = 0f;
+            m_MissHeartBeatCount = 0;
+        }
+
+        /// <summary>
+        /// 心跳间隔时长
+        /// </summary>
+        public float HeartBeatElapseSeconds
+        {
+            get => m_HeartBeatElapseSeconds;
+            set => m_HeartBeatElapseSeconds = value;
+        }
+
+        /// <summary>
+        /// 心跳丢失次数
+        /// </summary>
+        public int MissHeartBeatCount
+        {
+            get => m_MissHeartBeatCount;
+            set => m_MissHeartBeatCount = value;
+        }
+
+        /// <summary>
+        /// 重置心跳数据=>保活
+        /// </summary>
+        /// <param name="resetHeartBeatElapseSeconds">是否重置心跳流逝时长</param>
+        public void Reset(bool resetHeartBeatElapseSeconds = true)
+        {
+            if (resetHeartBeatElapseSeconds)
+            {
+                m_HeartBeatElapseSeconds = 0f;
+            }
+
+            m_MissHeartBeatCount = 0;
+        }
+    }
+
     public class NetChannel
     {
         static readonly NLog.Logger LOGGER = NLog.LogManager.GetCurrentClassLogger();
@@ -13,11 +60,17 @@ namespace Server.Core.Net.Tcp.Codecs
         public ProtocolReader Reader { get; protected set; }
         protected ProtocolWriter Writer { get; set; }
 
+        /// <summary>
+        /// 心跳状态
+        /// </summary>
+        public HeartBeatStatus HeartBeatStatus { get; }
+
         public IProtocal<NetMessage> Protocol { get; protected set; }
 
         public NetChannel(ConnectionContext context, IProtocal<NetMessage> protocal)
         {
             Context = context;
+            HeartBeatStatus = new HeartBeatStatus();
             Reader = context.CreateReader();
             Writer = context.CreateWriter();
             Protocol = protocal;
@@ -48,7 +101,7 @@ namespace Server.Core.Net.Tcp.Codecs
         public long GetSessionId()
         {
             if (Context.Items.TryGetValue(SESSIONID, out var idObj))
-                return (long) idObj;
+                return (long)idObj;
             return 0;
         }
 
@@ -64,6 +117,7 @@ namespace Server.Core.Net.Tcp.Codecs
             if (Writer != null)
             {
                 await Writer.WriteAsync(Protocol, msg);
+                HeartBeatStatus.Reset(true);
             }
         }
 
