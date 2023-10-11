e08d60eba33957450da747d5e53dae2520cd43b5
Blank <wangfj11@foxmail.com>
2023-10-11 21:05:06 +0800

    [增加]1.增加全局配置文件组件的注释

--- Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/GlobalConfig/GlobalConfigComponent.cs
+++ Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/GlobalConfig/GlobalConfigComponent.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityGameFramework.Runtime;
 
 namespace UnityGameFramework.Runtime
 {
@@ -10,9 +9,24 @@ namespace UnityGameFramework.Runtime
     [AddComponentMenu("Game Framework/Global Config")]
     public sealed class GlobalConfigComponent : GameFrameworkComponent
     {
+        /// <summary>
+        /// 检测App版本地址接口
+        /// </summary>
         public string CheckAppVersionUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 检测资源版本地址接口
+        /// </summary>
         public string CheckResourceVersionUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 附加内容
+        /// </summary>
         public string Content { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 主机服务地址
+        /// </summary>
         public string HostServerUrl { get; set; } = string.Empty;
     }
 }
