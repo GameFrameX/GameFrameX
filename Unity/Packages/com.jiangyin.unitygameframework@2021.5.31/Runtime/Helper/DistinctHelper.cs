417fd3495d4625007dacb0812bdfb84b1732547c
Blank <wangfj11@foxmail.com>
2023-10-11 21:15:06 +0800

    [增加]1. 增加去重帮助类的注释

--- Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/Helper/DistinctHelper.cs
+++ Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/Helper/DistinctHelper.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 
 namespace UnityGameFramework.Runtime
 {
+    /// <summary>
+    /// 去重。帮助类
+    /// </summary>
     public static class DistinctHelper
     {
+        /// <summary>
+        /// 根据条件去重
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <returns></returns>
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
         {
             var identifiedKeys = new HashSet<TKey>();
