0a5fbf5d2ff262cfe3a0088b32dbf65e638fa261
Blank <wangfj11@foxmail.com>
2023-10-11 22:40:57 +0800

    [修复]1. 修复高版本的unity 调试器不能断点的问题

--- Unity/Assets/Scripts/Framework/Procedure/HotfixHelper.cs
+++ Unity/Assets/Scripts/Framework/Procedure/HotfixHelper.cs
@@ -8,15 +8,25 @@ namespace UnityGameFramework.Procedure
     {
         public static async void StartHotfix()
         {
-            var assetHotfixProtoPath = AssetUtility.GetCodePath("Unity.Hotfix.Proto.dll");
-            var assetHotfixProtoOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixProtoPath);
-            var assemblyDataHotfixProto = assetHotfixProtoOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
-            Assembly.Load(assemblyDataHotfixProto);
+            var assetHotfixProtoDllPath = AssetUtility.GetCodePath("Unity.Hotfix.Proto.dll");
+            var assetHotfixProtoDllOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixProtoDllPath);
+            var assemblyDataHotfixProtoDll = assetHotfixProtoDllOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
 
-            var assetHotfixPath = AssetUtility.GetCodePath("Unity.Hotfix.dll");
-            var assetHotfixOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixPath);
-            var assemblyDataHotfix = assetHotfixOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
-            var ass = Assembly.Load(assemblyDataHotfix);
+            var assetHotfixProtoPdbPath = AssetUtility.GetCodePath("Unity.Hotfix.Proto.pdb");
+            var assetHotfixProtoPdbOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixProtoPdbPath);
+            var assemblyDataHotfixProtoPdb = assetHotfixProtoPdbOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
+
+            Assembly.Load(assemblyDataHotfixProtoDll, assemblyDataHotfixProtoPdb);
+
+            var assetHotfixDllPath = AssetUtility.GetCodePath("Unity.Hotfix.dll");
+            var assetHotfixDllOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixDllPath);
+            var assemblyDataHotfixDll = assetHotfixDllOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
+
+            var assetHotfixPdbPath = AssetUtility.GetCodePath("Unity.Hotfix.pdb");
+            var assetHotfixPdbOperationHandle = await GameApp.Asset.LoadAssetAsync<UnityEngine.Object>(assetHotfixPdbPath);
+            var assemblyDataHotfixPdb = assetHotfixPdbOperationHandle.GetAssetObject<UnityEngine.TextAsset>().bytes;
+
+            var ass = Assembly.Load(assemblyDataHotfixDll, assemblyDataHotfixPdb);
             var entryType = ass.GetType("Hotfix.HotfixLauncher");
             var method = entryType.GetMethod("Main");
             method?.Invoke(null, null);
