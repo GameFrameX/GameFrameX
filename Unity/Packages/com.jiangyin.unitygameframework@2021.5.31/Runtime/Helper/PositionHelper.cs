35f48a7de33affe77b5f1cbae9fbf123472d72fd
Blank <wangfj11@foxmail.com>
2023-10-11 21:33:02 +0800

    [增加]1. 增加坐标帮助类的注释

--- Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/Helper/PositionHelper.cs
+++ Unity/Packages/com.jiangyin.unitygameframework@2021.5.31/Runtime/Helper/PositionHelper.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace UnityGameFramework.Runtime
 {
+    /// <summary>
+    /// 坐标帮助类
+    /// </summary>
     public static class PositionHelper
     {
         public static Vector3 RayCastV2ToV3(Vector2 pos)
@@ -59,9 +62,9 @@ namespace UnityGameFramework.Runtime
         /// <summary>
         ///  求点到直线的距离，采用数学公式Ax+By+C = 0; d = A*p.x + B * p.y + C / sqrt(A^2 + B ^ 2)
         /// </summary>
-        /// <param name="startPoint"></param>
-        /// <param name="endPoint"></param>
-        /// <param name="point"></param>
+        /// <param name="startPoint">线的起点</param>
+        /// <param name="endPoint">线的终点</param>
+        /// <param name="point">点</param>
         /// <returns></returns>
         public static float DistanceOfPointToVector(Vector3 startPoint, Vector3 endPoint, Vector3 point)
         {
@@ -79,20 +82,20 @@ namespace UnityGameFramework.Runtime
         /// <summary>
         /// 判断射线是否碰撞到球体，如果碰撞到，返回射线起点到碰撞点之间的距离
         /// </summary>
-        /// <param name="ray"></param>
-        /// <param name="center"></param>
-        /// <param name="redius"></param>
-        /// <param name="dist"></param>
+        /// <param name="ray">射线</param>
+        /// <param name="center">中心点</param>
+        /// <param name="redis">半径</param>
+        /// <param name="dist">距离</param>
         /// <returns></returns>
-        public static bool RayCastSphere(Ray ray, Vector3 center, float redius, out float dist)
+        public static bool RayCastSphere(Ray ray, Vector3 center, float redis, out float dist)
         {
             dist = 0;
             Vector3 ma = center - ray.origin;
             float distance = Vector3.Cross(ma, ray.direction).magnitude / ray.direction.magnitude;
-            if (distance < redius)
+            if (distance < redis)
             {
                 float op = PythagoreanTheorem(Vector3.Distance(center, ray.origin), distance);
-                float rp = PythagoreanTheorem(redius, distance);
+                float rp = PythagoreanTheorem(redis, distance);
                 dist = op - rp;
                 return true;
             }
