8c544deed89d5bd7472d99075c94cbde6697c60d
AlianBlank <wangfj11@foxmail.com>
2023-10-30 16:45:15 +0800

    [增加]1. 增加GameAnalysis 的接口定义

---
+++ Unity/Packages/com.alianblank.gameframex/Runtime/GameAnalytics/GameAnalytics/IGameAnalyticsManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace GameFrameX.GameAnalytics
+{
+    /// <summary>
+    /// 游戏数据分析组件。
+    /// </summary>
+    public interface IGameAnalyticsManager
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        void Init();
+
+        /// <summary>
+        /// 开始计时
+        /// </summary>
+        /// <param name="eventName"></param>
+        void StartTimer(string eventName);
+
+        /// <summary>
+        /// 结束计时
+        /// </summary>
+        /// <param name="eventName"></param>
+        void StopTimer(string eventName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"></param>
+        void DesignEvent(string eventName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="eventValue"></param>
+        void DesignEvent(string eventName, float eventValue);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="customF"></param>
+        void DesignEvent(string eventName, Dictionary<string, string> customF);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="eventValue"></param>
+        /// <param name="customF"></param>
+        void DesignEvent(string eventName, float eventValue, Dictionary<string, string> customF);
+    }
+}
