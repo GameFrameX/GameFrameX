ba571e483845269320e9a15a30be9c860f391c15
Blank <wangfj11@foxmail.com>
2023-11-02 23:46:25 +0800

    [增加]1. 增加双向字典的删除函数

--- Unity/Packages/com.alianblank.gameframex/Runtime/Extension/Extension/BidirectionalDictionary.cs
+++ Unity/Packages/com.alianblank.gameframex/Runtime/Extension/Extension/BidirectionalDictionary.cs
@@ -2,37 +2,61 @@
 {
     public class BidirectionalDictionary<TKey, TValue>
     {
-        private Dictionary<TKey, TValue> forwardDictionary;
-        private Dictionary<TValue, TKey> reverseDictionary;
+        private readonly Dictionary<TKey, TValue> _forwardDictionary;
+        private readonly Dictionary<TValue, TKey> _reverseDictionary;
 
-        public BidirectionalDictionary()
+        public BidirectionalDictionary(int capacity = 8)
         {
-            forwardDictionary = new Dictionary<TKey, TValue>();
-            reverseDictionary = new Dictionary<TValue, TKey>();
+            _forwardDictionary = new Dictionary<TKey, TValue>(capacity);
+            _reverseDictionary = new Dictionary<TValue, TKey>(capacity);
         }
 
         public bool TryGetKey(TValue value, out TKey key)
         {
-            return reverseDictionary.TryGetValue(value, out key);
+            return _reverseDictionary.TryGetValue(value, out key);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            return forwardDictionary.TryGetValue(key, out value);
+            return _forwardDictionary.TryGetValue(key, out value);
         }
 
         public void Clear()
         {
-            forwardDictionary.Clear();
-            reverseDictionary.Clear();
+            _forwardDictionary.Clear();
+            _reverseDictionary.Clear();
         }
 
         public bool TryAdd(TKey key, TValue value)
         {
-            if (!forwardDictionary.ContainsKey(key))
+            if (!_forwardDictionary.ContainsKey(key))
             {
-                forwardDictionary.Add(key, value);
-                reverseDictionary.Add(value, key);
+                _forwardDictionary.Add(key, value);
+                _reverseDictionary.Add(value, key);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryRemoveByKey(TKey key)
+        {
+            if (_forwardDictionary.TryGetValue(key, out var value))
+            {
+                _forwardDictionary.Remove(key);
+                _reverseDictionary.Remove(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TryRemoveByValue(TValue value)
+        {
+            if (_reverseDictionary.TryGetValue(value, out var key))
+            {
+                _reverseDictionary.Remove(value);
+                _forwardDictionary.Remove(key);
                 return true;
             }
 
