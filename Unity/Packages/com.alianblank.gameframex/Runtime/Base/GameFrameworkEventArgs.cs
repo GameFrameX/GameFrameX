f30bcff593ef10eaed6ed8d045aa11a3eddb7609
Blank <wangfj11@foxmail.com>
2023-11-16 23:03:37 +0800

    [修改]1. 修改框架事件基类的构造函数为子类

--- Unity/Packages/com.alianblank.gameframex/Runtime/Base/GameFrameworkEventArgs.cs
+++ Unity/Packages/com.alianblank.gameframex/Runtime/Base/GameFrameworkEventArgs.cs
@@ -17,7 +17,7 @@ namespace GameFrameX
         /// <summary>
         /// 初始化游戏框架中包含事件数据的类的新实例。
         /// </summary>
-        public GameFrameworkEventArgs()
+        protected GameFrameworkEventArgs()
         {
         }
 
