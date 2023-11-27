2ee59bcb34079bd7d8f43bccd1d95f3845f2b9ea
AlianBlank <wangfj11@foxmail.com>
2023-11-27 16:53:57 +0800

    [增加]1. 增加资源是否加载成功的属性

--- Unity/Packages/com.tuyoogame.yooasset@1.4.3/Runtime/AssetSystem/Handles/OperationHandleBase.cs
+++ Unity/Packages/com.tuyoogame.yooasset@1.4.3/Runtime/AssetSystem/Handles/OperationHandleBase.cs
@@ -3,163 +3,177 @@ using System.Collections;
 
 namespace YooAsset
 {
-	public abstract class OperationHandleBase : IEnumerator
-	{
-		private readonly AssetInfo _assetInfo;
-		internal ProviderBase Provider { private set; get; }
+    public abstract class OperationHandleBase : IEnumerator
+    {
+        private readonly AssetInfo _assetInfo;
+        internal ProviderBase Provider { private set; get; }
 
-		internal OperationHandleBase(ProviderBase provider)
-		{
-			Provider = provider;
-			_assetInfo = provider.MainAssetInfo;
-		}
-		internal abstract void InvokeCallback();
+        internal OperationHandleBase(ProviderBase provider)
+        {
+            Provider = provider;
+            _assetInfo = provider.MainAssetInfo;
+        }
 
-		/// <summary>
-		/// 获取资源信息
-		/// </summary>
-		public AssetInfo GetAssetInfo()
-		{
-			return _assetInfo;
-		}
+        internal abstract void InvokeCallback();
 
-		/// <summary>
-		/// 获取下载报告
-		/// </summary>
-		public DownloadReport GetDownloadReport()
-		{
-			if (IsValidWithWarning == false)
-			{
-				return DownloadReport.CreateDefaultReport();
-			}
-			return Provider.GetDownloadReport();
-		}
+        /// <summary>
+        /// 获取资源信息
+        /// </summary>
+        public AssetInfo GetAssetInfo()
+        {
+            return _assetInfo;
+        }
 
-		/// <summary>
-		/// 当前状态
-		/// </summary>
-		public EOperationStatus Status
-		{
-			get
-			{
-				if (IsValidWithWarning == false)
-					return EOperationStatus.None;
-				if (Provider.Status == ProviderBase.EStatus.Failed)
-					return EOperationStatus.Failed;
-				else if (Provider.Status == ProviderBase.EStatus.Succeed)
-					return EOperationStatus.Succeed;
-				else
-					return EOperationStatus.None;
-			}
-		}
+        /// <summary>
+        /// 获取下载报告
+        /// </summary>
+        public DownloadReport GetDownloadReport()
+        {
+            if (IsValidWithWarning == false)
+            {
+                return DownloadReport.CreateDefaultReport();
+            }
 
-		/// <summary>
-		/// 最近的错误信息
-		/// </summary>
-		public string LastError
-		{
-			get
-			{
-				if (IsValidWithWarning == false)
-					return string.Empty;
-				return Provider.LastError;
-			}
-		}
+            return Provider.GetDownloadReport();
+        }
 
-		/// <summary>
-		/// 加载进度
-		/// </summary>
-		public float Progress
-		{
-			get
-			{
-				if (IsValidWithWarning == false)
-					return 0;
-				return Provider.Progress;
-			}
-		}
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public bool IsSucceed
+        {
+            get { return Status == EOperationStatus.Succeed; }
+        }
 
-		/// <summary>
-		/// 是否加载完毕
-		/// </summary>
-		public bool IsDone
-		{
-			get
-			{
-				if (IsValidWithWarning == false)
-					return false;
-				return Provider.IsDone;
-			}
-		}
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        public EOperationStatus Status
+        {
+            get
+            {
+                if (IsValidWithWarning == false)
+                    return EOperationStatus.None;
+                if (Provider.Status == ProviderBase.EStatus.Failed)
+                    return EOperationStatus.Failed;
+                else if (Provider.Status == ProviderBase.EStatus.Succeed)
+                    return EOperationStatus.Succeed;
+                else
+                    return EOperationStatus.None;
+            }
+        }
 
-		/// <summary>
-		/// 句柄是否有效
-		/// </summary>
-		public bool IsValid
-		{
-			get
-			{
-				if (Provider != null && Provider.IsDestroyed == false)
-					return true;
-				else
-					return false;
-			}
-		}
+        /// <summary>
+        /// 最近的错误信息
+        /// </summary>
+        public string LastError
+        {
+            get
+            {
+                if (IsValidWithWarning == false)
+                    return string.Empty;
+                return Provider.LastError;
+            }
+        }
 
-		/// <summary>
-		/// 句柄是否有效
-		/// </summary>
-		internal bool IsValidWithWarning
-		{
-			get
-			{
-				if (Provider != null && Provider.IsDestroyed == false)
-				{
-					return true;
-				}
-				else
-				{
-					if (Provider == null)
-						YooLogger.Warning($"Operation handle is released : {_assetInfo.AssetPath}");
-					else if (Provider.IsDestroyed)
-						YooLogger.Warning($"Provider is destroyed : {_assetInfo.AssetPath}");
-					return false;
-				}
-			}
-		}
+        /// <summary>
+        /// 加载进度
+        /// </summary>
+        public float Progress
+        {
+            get
+            {
+                if (IsValidWithWarning == false)
+                    return 0;
+                return Provider.Progress;
+            }
+        }
 
-		/// <summary>
-		/// 释放句柄
-		/// </summary>
-		internal void ReleaseInternal()
-		{
-			if (IsValidWithWarning == false)
-				return;
-			Provider.ReleaseHandle(this);
-			Provider = null;
-		}
+        /// <summary>
+        /// 是否加载完毕
+        /// </summary>
+        public bool IsDone
+        {
+            get
+            {
+                if (IsValidWithWarning == false)
+                    return false;
+                return Provider.IsDone;
+            }
+        }
 
-		#region 异步操作相关
-		/// <summary>
-		/// 异步操作任务
-		/// </summary>
-		public System.Threading.Tasks.Task Task
-		{
-			get { return Provider.Task; }
-		}
+        /// <summary>
+        /// 句柄是否有效
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                if (Provider != null && Provider.IsDestroyed == false)
+                    return true;
+                else
+                    return false;
+            }
+        }
 
-		// 协程相关
-		bool IEnumerator.MoveNext()
-		{
-			return !IsDone;
-		}
-		void IEnumerator.Reset()
-		{
-		}
-		object IEnumerator.Current
-		{
-			get { return Provider; }
-		}
-		#endregion
-	}
+        /// <summary>
+        /// 句柄是否有效
+        /// </summary>
+        internal bool IsValidWithWarning
+        {
+            get
+            {
+                if (Provider != null && Provider.IsDestroyed == false)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (Provider == null)
+                        YooLogger.Warning($"Operation handle is released : {_assetInfo.AssetPath}");
+                    else if (Provider.IsDestroyed)
+                        YooLogger.Warning($"Provider is destroyed : {_assetInfo.AssetPath}");
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 释放句柄
+        /// </summary>
+        internal void ReleaseInternal()
+        {
+            if (IsValidWithWarning == false)
+                return;
+            Provider.ReleaseHandle(this);
+            Provider = null;
+        }
+
+        #region 异步操作相关
+
+        /// <summary>
+        /// 异步操作任务
+        /// </summary>
+        public System.Threading.Tasks.Task Task
+        {
+            get { return Provider.Task; }
+        }
+
+        // 协程相关
+        bool IEnumerator.MoveNext()
+        {
+            return !IsDone;
+        }
+
+        void IEnumerator.Reset()
+        {
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Provider; }
+        }
+
+        #endregion
+    }
 }
