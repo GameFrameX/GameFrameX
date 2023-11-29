5d336355a41ca235bea153f7f73fe7a4249dbd92
Blank <wangfj11@foxmail.com>
2023-11-30 00:07:45 +0800

    [增加]1. 增加代码注释

--- Server/Server.Hotfix/Common/Handler/NetChannelExtensions.cs
+++ Server/Server.Hotfix/Common/Handler/NetChannelExtensions.cs
@@ -9,6 +9,14 @@ namespace Server.Hotfix.Common.Handler;
 
 public static class NetChannelExtensions
 {
+    /// <summary>
+    /// 将消息对象异步写入网络通道。
+    /// </summary>
+    /// <param name="channel">网络通道。</param>
+    /// <param name="msg">消息对象。</param>
+    /// <param name="uniId">唯一ID。</param>
+    /// <param name="code">状态码。</param>
+    /// <param name="desc">描述。</param>
     public static void WriteAsync(this NetChannel channel, MessageObject msg, int uniId, StateCode code = StateCode.Success, string desc = "")
     {
         if (msg != null)
--- Server/Server.Launcher/Common/Event/EventAttribute.cs
+++ Server/Server.Launcher/Common/Event/EventAttribute.cs
@@ -2,9 +2,16 @@
 
 namespace Server.Launcher.Common.Event
 {
+    /// <summary>
+    /// 表示事件的特性。
+    /// </summary>
     public class EventAttribute : EventInfoAttribute
     {
-        public EventAttribute(EventId eventId) : base((int) eventId)
+        /// <summary>
+        /// 使用指定的事件ID初始化 <see cref="EventAttribute"/> 类的新实例。
+        /// </summary>
+        /// <param name="eventId">事件ID。</param>
+        public EventAttribute(EventId eventId) : base((int)eventId)
         {
         }
     }
