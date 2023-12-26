a25c8a2c84d134ae70ede06f7eeef633800753ed
AlianBlank <wangfj11@foxmail.com>
2023-12-26 11:53:55 +0800

    [增加]1. 增加重载热更新的HTTP接口

---
+++ Server/Server.Hotfix/Http/HttpReloadHandler.cs
@@ -0,0 +1,33 @@
+using Server.NetWork.HTTP;
+
+namespace Server.Hotfix.Http;
+
+/// <summary>
+/// 热更新
+/// http://localhost:20001/game/api?command=Reload&amp;version=1.0.0
+/// </summary>
+[HttpMsgMapping(typeof(HttpReloadHandler))]
+public class HttpReloadHandler : BaseHttpHandler
+{
+    /// <summary>
+    /// ***正式的HttpHandler请一定设置CheckSign为True***
+    /// </summary>
+    public override bool IsCheckSign => false;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <param name="url"></param>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    public override async Task<string> Action(string ip, string url, Dictionary<string, string> parameters)
+    {
+        if (parameters.TryGetValue("version", out var version))
+        {
+            await HotfixMgr.LoadHotfixModule(version);
+        }
+
+        return Task.FromResult("ok").Result;
+    }
+}
