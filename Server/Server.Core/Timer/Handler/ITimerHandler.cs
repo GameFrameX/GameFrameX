6efb3de80c7f1ebba7fd833bb4f1e4eb45c4bc89
AlianBlank <wangfj11@foxmail.com>
2024-01-15 18:00:31 +0800

    [增加]1. 增加计时器相关的代码注释

--- Server/Server.Core/Timer/Handler/ITimerHandler.cs
+++ Server/Server.Core/Timer/Handler/ITimerHandler.cs
@@ -3,8 +3,17 @@ using Server.Utility;
 
 namespace Server.Core.Timer.Handler
 {
+    /// <summary>
+    /// 计时器处理器接口
+    /// </summary>
     public interface ITimerHandler
     {
-        Task InnerHandleTimer(IComponentAgent actor, Param param);
+        /// <summary>
+        /// 内部计时器处理器调用函数
+        /// </summary>
+        /// <param name="agent">组件代理对象</param>
+        /// <param name="param">参数</param>
+        /// <returns></returns>
+        Task InnerHandleTimer(IComponentAgent agent, Param param);
     }
 }
--- Server/Server.Core/Timer/Handler/NotHotfixTimerHandler.cs
+++ Server/Server.Core/Timer/Handler/NotHotfixTimerHandler.cs
@@ -3,6 +3,9 @@ using Server.Utility;
 
 namespace Server.Core.Timer.Handler
 {
+    /// <summary>
+    /// 非热更程序集的计时器处理器，不需要热更时间更新
+    /// </summary>
     public abstract class NotHotfixTimerHandler : IJob
     {
         public Task Execute(IJobExecutionContext context)
