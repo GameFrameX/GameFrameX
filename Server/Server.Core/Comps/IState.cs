d61edb06040e9d3d9004a9b62f53e350b83b11a8
AlianBlank <wangfj11@foxmail.com>
2024-01-15 18:00:56 +0800

    [增加]1. 增加状态接口注释

--- Server/Server.Core/Comps/IState.cs
+++ Server/Server.Core/Comps/IState.cs
@@ -1,7 +1,14 @@
 ﻿namespace Server.Core.Comps
 {
+    /// <summary>
+    /// 状态接口
+    /// </summary>
     public interface IState
     {
+        /// <summary>
+        /// 准备状态
+        /// </summary>
+        /// <returns></returns>
         public Task ReadStateAsync();
     }
 }
