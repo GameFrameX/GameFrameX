6090356bb112281b07aa4efd8994a623c334f31a
AlianBlank <wangfj11@foxmail.com>
2024-01-17 20:21:35 +0800

    [修改]1. 修改编译警告

--- Unity/Packages/com.alianblank.gameframex/Runtime/Utility/Utility.Verifier.Crc64.cs
+++ Unity/Packages/com.alianblank.gameframex/Runtime/Utility/Utility.Verifier.Crc64.cs
@@ -90,9 +90,10 @@ namespace GameFrameX
                     _crc = InitialState;
                 }
 
-                /// <summary>Gets the current computed hash value without modifying accumulated state.</summary>
+                /// <summary>
+                /// Gets the current computed hash value without modifying accumulated state.
+                /// </summary>
                 /// <returns>The hash value for the data already provided.</returns>
-                [CLSCompliant(false)]
                 public ulong GetCurrentHashAsUInt64() => _crc;
 
                 /// <summary>
@@ -175,7 +176,6 @@ namespace GameFrameX
                 /// <summary>Computes the CRC-64 hash of the provided data.</summary>
                 /// <param name="source">The data to hash.</param>
                 /// <returns>The computed CRC-64 hash.</returns>
-                [CLSCompliant(false)]
                 public static ulong HashToUInt64(ReadOnlySpan<byte> source) =>
                     Update(InitialState, source);
 
