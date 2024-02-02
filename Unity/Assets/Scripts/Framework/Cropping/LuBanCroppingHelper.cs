62c6aa4b9af597ba22677c17212000597c30eb6c
AlianBlank <wangfj11@foxmail.com>
2024-02-02 18:25:14 +0800

    [增加]1. 增加防裁剪的调用帮助类

---
+++ Unity/Assets/Scripts/Framework/Cropping/LuBanCroppingHelper.cs
@@ -0,0 +1,16 @@
+﻿using LuBan.Runtime;
+using UnityEngine;
+
+public class LuBanCroppingHelper : MonoBehaviour
+{
+    void Start()
+    {
+        _ = typeof(BeanBase);
+        _ = typeof(ByteBuf);
+        _ = typeof(SerializationException);
+        _ = typeof(SegmentSaveState);
+        _ = typeof(EDeserializeError);
+        _ = typeof(StringUtil);
+        _ = typeof(ITypeId);
+    }
+}
