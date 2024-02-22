8c7cd84754a462fe18f2351246f39e4fb08a8311
AlianBlank <wangfj11@foxmail.com>
2024-02-22 20:55:36 +0800

    [增加]1. 增加ProtoBuff 库的帮助函数

--- Server/Server.Serialize/Serialize/SerializerHelper.cs
+++ Server/Server.Serialize/Serialize/SerializerHelper.cs
@@ -1,34 +1,62 @@
 using System.Buffers;
-using MessagePack;
+using ProtoBuf.Meta;
 
 namespace Server.Serialize.Serialize
 {
     public static class SerializerHelper
     {
+        /// <summary>
+        /// 序列化对象
+        /// </summary>
+        /// <param name="value"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static byte[] Serialize<T>(T value)
         {
-            return MessagePackSerializer.Serialize(value);
+            return ProtoBuf.SerializerHelper.Serialize<T>(value);
         }
 
-        public static void Serialize<T>(IBufferWriter<byte> writer,
-            T value,
-            MessagePackSerializerOptions? options = null,
-            CancellationToken cancellationToken = default(CancellationToken))
+        /// <summary>
+        /// 序列化对象
+        /// </summary>
+        /// <param name="bufferWriter"></param>
+        /// <param name="value"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Serialize<T>(IBufferWriter<byte> bufferWriter, T value)
         {
-            MessagePackSerializer.Serialize(writer, value, options, cancellationToken);
+            ProtoBuf.SerializerHelper.Serialize<T>(ref bufferWriter, value);
         }
 
-        public static T Deserialize<T>(byte[] data)
+        public static void Register<T>()
         {
-            return MessagePackSerializer.Deserialize<T>(data);
+            ProtoBuf.SerializerHelper.Register<T>();
         }
 
-        public static T Deserialize<T>(
-            in ReadOnlySequence<byte> byteSequence,
-            MessagePackSerializerOptions? options = null,
-            CancellationToken cancellationToken = default(CancellationToken))
+        public static void Register(Type type)
         {
-            return MessagePackSerializer.Deserialize<T>(byteSequence, options, cancellationToken);
+            ProtoBuf.SerializerHelper.Register(type);
+        }
+
+        /// <summary>
+        /// 反序列化数据对象
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="type">对象类型</param>
+        /// <returns></returns>
+        public static object Deserialize(byte[] data, Type type)
+        {
+            return ProtoBuf.SerializerHelper.Deserialize(data, type);
+        }
+
+        /// <summary>
+        /// 反序列化数据对象
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="type">对象类型</param>
+        /// <returns></returns>
+        public static object Deserialize(ReadOnlySequence<byte> data, Type type)
+        {
+            return ProtoBuf.SerializerHelper.Deserialize(ref data, type);
         }
     }
 }
