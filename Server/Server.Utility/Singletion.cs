5249f2baa2f48cfd362b1eb12a282c51f42d7faf
AlianBlank <wangfj11@foxmail.com>
2024-02-28 14:53:06 +0800

    [增加]1. 增加单例基类

---
+++ Server/Server.Utility/Singletion.cs
@@ -0,0 +1,37 @@
+﻿namespace Server.Utility
+{
+    /// <summary>
+    /// 单例类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Singleton<T> where T : Singleton<T>, new()
+    {
+        private static T? _instance;
+        private static readonly object LockObject = new object();
+
+        /// <summary>
+        /// 单例对象
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                lock (LockObject)
+                {
+                    if (_instance == null)
+                    {
+                        lock (LockObject)
+                        {
+                            if (_instance == null)
+                            {
+                                _instance = new T();
+                            }
+                        }
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+    }
+}
