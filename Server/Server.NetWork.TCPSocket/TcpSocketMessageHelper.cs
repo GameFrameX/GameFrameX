901997711f239e356416f463ccb0a5098e578d46
AlianBlank <wangfj11@foxmail.com>
2024-02-28 14:59:45 +0800

    [优化]1. 优化消息处理帮助类

--- Server/Server.NetWork.TCPSocket/TcpSocketMessageHelper.cs
+++ Server/Server.NetWork.TCPSocket/TcpSocketMessageHelper.cs
@@ -1,27 +1,9 @@
 namespace Server.NetWork.TCPSocket
 {
-    public sealed class TcpSocketMessageHelper : IMessageHelper
+    public sealed class TcpSocketMessageHelper : BaseSocketMessageHelper
     {
-        /// <summary>
-        /// 消息处理器。根据消息Id查找消息的处理器
-        /// </summary>
-        public Func<int, IMessageHandler> MessageHandler { get; set; }
-
-        /// <summary>
-        /// 根据消息Id查找消息类型
-        /// </summary>
-        public Func<int, Type> MessageTypeByIdGetter { get; set; }
-
-        /// <summary>
-        /// 根据消息类型查找消息ID
-        /// </summary>
-        public Func<Type, int> MessageIdByTypeGetter { get; set; }
-
-        public TcpSocketMessageHelper(Func<int, IMessageHandler> messageHandler, Func<int, Type> typeGetter, Func<Type, int> idGetter)
+        public TcpSocketMessageHelper(Func<int, IMessageHandler> messageHandler, Func<int, Type> typeGetter, Func<Type, int> idGetter) : base(messageHandler, typeGetter, idGetter)
         {
-            MessageHandler = messageHandler;
-            MessageTypeByIdGetter = typeGetter;
-            MessageIdByTypeGetter = idGetter;
         }
     }
 }
--- Server/Server.NetWork.WebSocket/WebSocketMessageHelper.cs
+++ Server/Server.NetWork.WebSocket/WebSocketMessageHelper.cs
@@ -1,32 +1,16 @@
+using Server.NetWork.TCPSocket;
+
 namespace Server.NetWork.WebSocket;
 
-public sealed class WebSocketMessageHelper : IMessageHelper
+public sealed class WebSocketMessageHelper : BaseSocketMessageHelper
 {
-    /// <summary>
-    /// 消息处理器。根据消息Id查找消息的处理器
-    /// </summary>
-    public Func<int, IMessageHandler> MessageHandler { get; set; }
-
-    /// <summary>
-    /// 根据消息Id查找消息类型
-    /// </summary>
-    public Func<int, Type> MessageTypeByIdGetter { get; set; }
-
-    /// <summary>
-    /// 根据消息类型查找消息ID
-    /// </summary>
-    public Func<Type, int> MessageIdByTypeGetter { get; set; }
-
     /// <summary>
     /// 构造消息帮助类
     /// </summary>
     /// <param name="messageHandler">消息处理器</param>
     /// <param name="typeGetter"></param>
     /// <param name="idGetter"></param>
-    public WebSocketMessageHelper(Func<int, IMessageHandler> messageHandler, Func<int, Type> typeGetter, Func<Type, int> idGetter)
+    public WebSocketMessageHelper(Func<int, IMessageHandler> messageHandler, Func<int, Type> typeGetter, Func<Type, int> idGetter) : base(messageHandler, typeGetter, idGetter)
     {
-        MessageHandler = messageHandler;
-        MessageTypeByIdGetter = typeGetter;
-        MessageIdByTypeGetter = idGetter;
     }
 }
