3a3eeccdf13e0edad5a986b9b237905aa3bdd1e6
AlianBlank <wangfj11@foxmail.com>
2024-03-02 19:03:29 +0800

    [增加]1. 增加服务器管理程序集

---
+++ Server/Server.ServerManager/ServerStatus.cs
@@ -0,0 +1,19 @@
+namespace Server.ServerManager;
+
+public enum ServerStatus
+{
+    /// <summary>
+    /// 在线
+    /// </summary>
+    Online,
+
+    /// <summary>
+    /// 离线
+    /// </summary>
+    Offline,
+
+    /// <summary>
+    /// 未知  
+    /// </summary>
+    Unknown
+}
