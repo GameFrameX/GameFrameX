fc467c9abdcbe5c27892e6736a10efe19c891285
AlianBlank <wangfj11@foxmail.com>
2024-03-03 19:06:18 +0800

    [修改]1. 修改热更新逻辑更新代码

--- Server/Server.Hotfix/Common/HotfixBridge.cs
+++ Server/Server.Hotfix/Common/HotfixBridge.cs
@@ -34,15 +34,15 @@ namespace Server.Hotfix.Common
                 // Log.Debug("MsgID:" + messageId);
                 return ProtoMessageIdHandler.GetReqTypeById(messageId);
             });
-
+            var appSetting = GlobalSettings.GetSetting<AppSetting>(ServerType.Game);
             var webSocketMessageHelper = new WebSocketMessageHelper(HotfixMgr.GetTcpHandler, HotfixMgr.GetMsgTypeById, HotfixMgr.GetMsgIdByType);
-            await WebSocketServer.Start(GlobalSettings.WsPort, GlobalSettings.WssPort, GlobalSettings.WssCertFilePath, webSocketMessageHelper, new WebSocketChannelHandler());
+            await WebSocketServer.Start(appSetting.WsPort, appSetting.WssPort, appSetting.WssCertFilePath, webSocketMessageHelper, new WebSocketChannelHandler());
             Log.Info("WebSocket 服务启动完成...");
 
             var tcpSocketMessageHelper = new TcpSocketMessageHelper(HotfixMgr.GetTcpHandler, HotfixMgr.GetMsgTypeById, HotfixMgr.GetMsgIdByType);
-            await TcpServer.Start(GlobalSettings.TcpPort, tcpSocketMessageHelper, builder => { builder.UseConnectionHandler<AppTcpConnectionHandler>(); });
+            await TcpServer.Start(appSetting.TcpPort, tcpSocketMessageHelper, builder => { builder.UseConnectionHandler<AppTcpConnectionHandler>(); });
             Log.Info("tcp 服务启动完成...");
-            await HttpServer.Start(GlobalSettings.HttpPort, GlobalSettings.HttpsPort, HotfixMgr.GetHttpHandler);
+            await HttpServer.Start(appSetting.HttpPort, appSetting.HttpsPort, HotfixMgr.GetHttpHandler);
             Log.Info("load config data");
 
             GlobalTimer.Start();
