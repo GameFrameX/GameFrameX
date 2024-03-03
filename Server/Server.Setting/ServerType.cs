d2c3a468155abf524cf5dae3d76931c0f02a0499
AlianBlank <wangfj11@foxmail.com>
2024-03-03 15:30:13 +0800

    [修改]1. 修改服务器类型定义

--- Server/Server.Setting/ServerType.cs
+++ Server/Server.Setting/ServerType.cs
@@ -6,88 +6,88 @@ namespace Server.Setting;
 [Flags]
 public enum ServerType
 {
-    /// <summary>
-    /// 全部
-    /// </summary>
-    All = 0,
-
-    /// <summary>
-    /// 登录服务器
-    /// </summary>
-    Login,
-
-    ///<summary>
-    /// 游戏服
-    /// </summary>
-    Game,
-
-    ///<summary>
-    /// 中心服
-    /// </summary>
-    Center,
-
-    ///<summary>
-    /// 充值服
-    /// </summary>
-    Recharge,
-
-    ///<summary>
-    /// 远程备份
-    /// </summary>
-    Backup,
-
-    /// <summary>
-    /// 服务发现服
-    /// </summary>
-    Discovery,
-
-    /// <summary>
-    /// 网关服
-    /// </summary>
-    Gate,
-
-    /// <summary>
-    /// 逻辑服
-    /// </summary>
-    Logic,
-
-    /// <summary>
-    /// 聊天服
-    /// </summary>
-    Chat,
-
-    /// <summary>
-    /// 邮件服
-    /// </summary>
-    Mail,
-
-    /// <summary>
-    /// 公会服
-    /// </summary>
-    Guild,
-
-    /// <summary>
-    /// 账号服
-    /// </summary>
-    Account,
-
-    /// <summary>
-    /// 房间服
-    /// </summary>
-    Room,
+    #region 基础服务
 
     /// <summary>
     /// 日志服
     /// </summary>
-    Log,
+    Log = 1 << 1,
 
     /// <summary>
     /// 数据库
     /// </summary>
-    DataBase,
+    DataBase = Log << 1,
 
     /// <summary>
     /// 缓存服
     /// </summary>
-    Cache
+    Cache = DataBase << 1,
+
+    /// <summary>
+    /// 网关服
+    /// </summary>
+    Gate = Cache << 1,
+
+    /// <summary>
+    /// 账号服
+    /// </summary>
+    Account = Gate << 1,
+
+    /// <summary>
+    /// 服务发现服
+    /// </summary>
+    Discovery = Account << 1,
+
+    ///<summary>
+    /// 远程备份
+    /// </summary>
+    Backup = Discovery << 1,
+
+    #endregion
+
+    /// <summary>
+    /// 登录服务器
+    /// </summary>
+    Login = Backup << 1,
+
+    ///<summary>
+    /// 游戏服
+    /// </summary>
+    Game = Login << 1,
+
+
+    ///<summary>
+    /// 充值服
+    /// </summary>
+    Recharge = Game << 1,
+
+    /// <summary>
+    /// 逻辑服
+    /// </summary>
+    Logic = Recharge << 1,
+
+    /// <summary>
+    /// 聊天服
+    /// </summary>
+    Chat = Logic << 1,
+
+    /// <summary>
+    /// 邮件服
+    /// </summary>
+    Mail = Chat << 1,
+
+    /// <summary>
+    /// 公会服
+    /// </summary>
+    Guild = Mail << 1,
+
+    /// <summary>
+    /// 房间服
+    /// </summary>
+    Room = Guild << 1,
+
+    /// <summary>
+    /// 全部
+    /// </summary>
+    All = Room | Game | Logic | Recharge | Chat | Mail | Guild | Backup | Account | Discovery | Gate | Cache | DataBase | Log,
 }
