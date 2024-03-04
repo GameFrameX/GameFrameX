bec2df2e099880f4de4db6b8889f0406670b1640
AlianBlank <wangfj11@foxmail.com>
2024-03-04 19:29:12 +0800

    [修改]1. 修改消息编码规则

--- Server/Server.Launcher/Message/MessageEncoderHandler.cs
+++ Server/Server.Launcher/Message/MessageEncoderHandler.cs
@@ -16,7 +16,7 @@ class MessageEncoderHandler : IMessageEncoderHandler
         var bytes = SerializerHelper.Serialize(message);
 
         // len +timestamp + msgId + bytes.length
-        int len = 4 + 4 + 8 + 4 + bytes.Length;
+        int len = 4 + 8 + 4 + 4 + 4 + bytes.Length;
         var span = ArrayPool<byte>.Shared.Rent(len);
         int offset = 0;
         span.WriteInt(len, ref offset);
@@ -24,9 +24,6 @@ class MessageEncoderHandler : IMessageEncoderHandler
         var messageType = message.GetType();
         var msgId = ProtoMessageIdHandler.GetReqMessageIdByType(messageType);
         span.WriteInt(msgId, ref offset);
-        // var uniqueIdBytes = Encoding.UTF8.GetBytes(message.UniqueId);
-        // span.WriteInt(uniqueIdBytes.Length, ref offset);
-        // span.WriteBytesWithoutLength(uniqueIdBytes, ref offset);
         span.WriteInt(bytes.Length, ref offset);
         span.WriteBytesWithoutLength(bytes, ref offset);
         return span;
