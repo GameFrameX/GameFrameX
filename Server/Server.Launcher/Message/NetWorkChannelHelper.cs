9c17a521bb35570e342c8bb8fd6da403d69ae456
AlianBlank <wangfj11@foxmail.com>
2024-03-12 20:10:09 +0800

    [增加]1. 增加网络频道的帮助类

---
+++ Server/Server.Launcher/Message/NetWorkChannelHelper.cs
@@ -0,0 +1,32 @@
+﻿using Server.NetWork;
+using Server.NetWork.Messages;
+
+namespace Server.Launcher.Message;
+
+public class NetWorkChannelHelper : INetWorkChannelHelper
+{
+    /// <summary>
+    /// 发送消息
+    /// </summary>
+    public Func<IMessage, byte[]> OnSendMessage { get; set; }
+
+    /// <summary>
+    /// 收到消息
+    /// </summary>
+    public Action<ISession, byte[], long, long> OnReceiveMessage { get; set; }
+
+    /// <summary>
+    /// 网络连接断开
+    /// </summary>
+    public Action OnDisconnected { get; set; }
+
+    /// <summary>
+    /// 网络连接成功
+    /// </summary>
+    public Action OnConnected { get; set; }
+
+    /// <summary>
+    /// 网络错误
+    /// </summary>
+    public Action<string> OnError { get; set; }
+}
---
+++ Server/Server.NetWork/INetWorkChannelHelper.cs
@@ -0,0 +1,34 @@
+﻿using Server.NetWork.Messages;
+
+namespace Server.NetWork;
+
+/// <summary>
+/// 网络渠道帮助类
+/// </summary>
+public interface INetWorkChannelHelper
+{
+    /// <summary>
+    /// 发送消息
+    /// </summary>
+    Func<IMessage, byte[]> OnSendMessage { get; set; }
+
+    /// <summary>
+    /// 收到消息
+    /// </summary>
+    Action<ISession, byte[], long, long> OnReceiveMessage { get; set; }
+
+    /// <summary>
+    /// 网络连接断开
+    /// </summary>
+    Action? OnDisconnected { get; set; }
+
+    /// <summary>
+    /// 网络连接成功
+    /// </summary>
+    Action? OnConnected { get; set; }
+
+    /// <summary>
+    /// 网络错误
+    /// </summary>
+    Action<string>? OnError { get; set; }
+}
