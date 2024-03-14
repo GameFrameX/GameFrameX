b1ca6b669418a00a8041be8a4b18dfa94f1d46d4
AlianBlank <wangfj11@foxmail.com>
2024-03-14 21:03:28 +0800

    [增加]1. 增加存档类型的扫描

---
+++ Server/Server.Apps/CacheStateTypeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+using Server.DBServer.State;
+using Server.Extension;
+
+namespace Server.Apps;
+
+public static class CacheStateTypeManager
+{
+    static readonly ConcurrentDictionary<long, Type> hashMap = new ConcurrentDictionary<long, Type>();
+
+    /// <summary>
+    /// 初始化对象实体集的扫描
+    /// </summary>
+    public static void Init()
+    {
+        var types = typeof(AppsHandler).Assembly.GetTypes();
+        foreach (var type in types)
+        {
+            var isImplWithInterface = type.IsImplWithInterface(typeof(ICacheState));
+            if (isImplWithInterface)
+            {
+                hashMap.TryAdd(Utility.Hash.XXHash.Hash32(type.ToString()), type);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 根据类型ID获取类型
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public static Type? GetType(long id)
+    {
+        hashMap.TryGetValue(id, out var value);
+        return value;
+    }
+}
