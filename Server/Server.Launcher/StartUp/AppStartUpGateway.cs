ea822a9aea1fd2b5457b79de7a733ef5052dc720
AlianBlank <wangfj11@foxmail.com>
2024-03-16 20:00:08 +0800

    [增加]1. 增加网关的链接到中心服和接受路由的连接

--- Server/Server.Launcher/StartUp/AppStartUpGateway.cs
+++ Server/Server.Launcher/StartUp/AppStartUpGateway.cs
@@ -1,4 +1,9 @@
-﻿/// <summary>
+﻿using Server.Launcher.PipelineFilter;
+using Server.NetWork.TCPSocket;
+using SuperSocket.Channel;
+using ErrorEventArgs = SuperSocket.ClientEngine.ErrorEventArgs;
+
+/// <summary>
 /// 网关服务器
 /// </summary>
 [StartUpTag(ServerType.Gateway)]
@@ -13,24 +18,19 @@ internal sealed class AppStartUpGateway : AppStartUpBase
     {
         try
         {
-            LogHelper.Info($"启动服务器{ServerType} 开始! address: {Setting.CenterUrl}  port: {Setting.GrpcPort}");
-
+            LogHelper.Info($"启动服务器{ServerType} 开始! address: {Setting.LocalIp}  port: {Setting.TcpPort}");
+            await StartServer();
             client = new AsyncTcpSession();
             client.Connected += ClientOnConnected;
             client.Closed += ClientOnClosed;
             client.DataReceived += ClientOnDataReceived;
-
+            client.Error += ClientOnError;
 
             LogHelper.Info("开始链接到中心服务器 ...");
             client.Connect(new IPEndPoint(IPAddress.Parse(Setting.CenterUrl), Setting.GrpcPort));
             LogHelper.Info("链接完成!");
             TimeSpan delay = TimeSpan.FromSeconds(5);
             await Task.Delay(delay);
-
-            var timer = new System.Timers.Timer(5000);
-            timer.Elapsed += TimerOnElapsed;
-            timer.Enabled = true;
-            timer.Start();
 
             await AppExitToken;
             Console.Write("全部断开...");
@@ -44,7 +44,73 @@ internal sealed class AppStartUpGateway : AppStartUpBase
         }
     }
 
-    private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+
+    #region Server
+
+    IServer server;
+
+    private async Task StartServer()
+    {
+        server = SuperSocketHostBuilder.Create<IMessage, MessageObjectPipelineFilter>()
+            .ConfigureSuperSocket(ConfigureSuperSocket)
+            .UseSessionFactory<GameSessionFactory>()
+            .UseClearIdleSession()
+            .UsePackageDecoder<MessageActorDiscoveryDecoderHandler>()
+            .UsePackageEncoder<MessageActorDiscoveryEncoderHandler>()
+            .UseSessionHandler(OnConnected, OnDisconnected)
+            .UsePackageHandler(PackageHandler)
+            .UseInProcSessionContainer()
+            .BuildAsServer();
+
+        await server.StartAsync();
+    }
+
+
+    // readonly MessageActorDiscoveryEncoderHandler messageEncoderHandler = new MessageActorDiscoveryEncoderHandler();
+
+    private async ValueTask PackageHandler(IAppSession session, IMessage messageObject)
+    {
+        if (messageObject is MessageObject msg)
+        {
+            var messageId = msg.MessageId;
+            if (Setting.IsDebug && Setting.IsDebugReceive)
+            {
+                LogHelper.Debug($"---收到消息ID:[{messageId}] ==>消息类型:{msg.GetType()} 消息内容:{messageObject}");
+            }
+        }
+
+        // 发送
+        var response = new RespActorHeartBeat()
+        {
+            Timestamp = TimeHelper.UnixTimeSeconds()
+        };
+        // await session.SendAsync(messageEncoderHandler, response);
+    }
+
+
+    private ValueTask OnConnected(IAppSession appSession)
+    {
+        LogHelper.Info("有路由客户端网络连接成功！。链接信息：SessionID:" + appSession.SessionID + " RemoteEndPoint:" + appSession.RemoteEndPoint);
+        // NamingServiceManager.Instance.Add();
+        return ValueTask.CompletedTask;
+    }
+
+    private ValueTask OnDisconnected(object sender, CloseEventArgs args)
+    {
+        LogHelper.Info("有路由客户端网络断开连接成功！。断开信息：" + args.Reason);
+        return ValueTask.CompletedTask;
+    }
+
+    private void ConfigureSuperSocket(ServerOptions options)
+    {
+        options.AddListener(new ListenOptions { Ip = IPAddress.Any.ToString(), Port = Setting.TcpPort });
+    }
+
+    #endregion
+
+    #region Client
+
+    protected override void HeartBeatTimerOnElapsed(object sender, ElapsedEventArgs e)
     {
         //心跳包
         if (client.IsConnected)
@@ -52,11 +118,17 @@ internal sealed class AppStartUpGateway : AppStartUpBase
             reqHeartBeat.Timestamp = TimeHelper.UnixTimeSeconds();
             reqHeartBeat.UniqueId = UtilityIdGenerator.GetNextUniqueId();
             SendMessage(reqHeartBeat);
-        } //断线重连
-        else if (!client.IsConnected)
-        {
-            client.Connect(new IPEndPoint(IPAddress.Parse(Setting.CenterUrl), Setting.GrpcPort));
         }
+    }
+
+    protected override void ReconnectionTimerOnElapsed(object sender, ElapsedEventArgs e)
+    {
+        ConnectToDiscovery();
+    }
+
+    private void ConnectToDiscovery()
+    {
+        client.Connect(new IPEndPoint(IPAddress.Parse(Setting.CenterUrl), Setting.GrpcPort));
     }
 
     private void ClientOnDataReceived(object sender, DataEventArgs e)
@@ -73,11 +145,25 @@ internal sealed class AppStartUpGateway : AppStartUpBase
 
     private void ClientOnConnected(object sender, EventArgs e)
     {
+        HeartBeatTimer.Start();
+        ReconnectionTimer.Stop();
     }
 
     private void ClientOnClosed(object sender, EventArgs eventArgs)
     {
         LogHelper.Info("网络连接断开");
+        HeartBeatTimer.Stop();
+        ReconnectionTimer.Start();
+        ConnectToDiscovery();
+    }
+
+    private void ClientOnError(object sender, ErrorEventArgs e)
+    {
+        LogHelper.Info("链接到中心服务器失败");
+        // 开启重连
+        HeartBeatTimer.Stop();
+        ReconnectionTimer.Start();
+        ConnectToDiscovery();
     }
 
     private void SendMessage(IMessage message)
@@ -91,6 +177,8 @@ internal sealed class AppStartUpGateway : AppStartUpBase
         client.TrySend(span);
         ArrayPool<byte>.Shared.Return(span);
     }
+
+    #endregion
 
 
     protected override void Init()
