eb30f80fb697329a41d701c48ffefc6b9a202d98
AlianBlank <wangfj11@foxmail.com>
2024-03-19 19:50:45 +0800

    [增加]1. 增加当前运行平台判断帮助类

---
+++ Server/Server.Utility/PlatformRuntimeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Server.Utility;
+
+public static class PlatformRuntimeHelper
+{
+    public static bool IsLinux
+    {
+        get
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+    }
+    public static bool IsOSX
+    {
+        get
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        }
+    }
+    public static bool IsWindows
+    {
+        get
+        {
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        }
+    }
+}
