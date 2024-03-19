eb98bc937a8546b1082589fd455baa4638f78223
AlianBlank <wangfj11@foxmail.com>
2024-03-19 20:04:17 +0800

    [增加]1. 增加消息类型

---
+++ Server/Server.NetWork/Messages/MessageType.cs
@@ -0,0 +1,29 @@
+﻿namespace Server.NetWork.Messages;
+
+/// <summary>
+/// 消息类型
+/// </summary>
+public enum MessageType : byte
+{
+    Unknown = 0,
+
+    /// <summary>
+    /// 请求
+    /// </summary>
+    Request = 1,
+
+    /// <summary>
+    /// 返回
+    /// </summary>
+    Response = 2,
+
+    /// <summary>
+    /// Actor请求
+    /// </summary>
+    ActorRequest = 3,
+
+    /// <summary>
+    /// Actor返回
+    /// </summary>
+    ActorResponse = 4
+}
