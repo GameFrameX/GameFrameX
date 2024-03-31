b6c73184ece3299d15dd3d332d0ce1c010ef6966
AlianBlank <wangfj11@foxmail.com>
2024-03-31 16:54:01 +0800

    [增加]1. 增加本地化处理

---
+++ Config/gen-server-json.sh
@@ -0,0 +1,2 @@
+dotnet ./Tools/Luban.dll --target server --dataTarget json --codeTarget cs-dotnet-json --xargs outputDataDir=../Server//Server.Config/Json  --xargs outputCodeDir=../Server//Server.Config/Config --conf ./Luban.conf
+pause
