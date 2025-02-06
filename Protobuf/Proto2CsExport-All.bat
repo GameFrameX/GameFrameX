3cfb58e2a63c2fc6f54aecf4a690dd04c807fe64
Blank <wangfj11@foxmail.com>
2025-02-06 15:27:08 +0800

    [增加]1. 增加一键导出双端协议的快捷命令

---
+++ Protobuf/Proto2CsExport-All.bat
@@ -0,0 +1,3 @@
+@echo off
+start Proto2CsExport_Client.bat
+start Proto2CsExport_Server.bat
--- Protobuf/Proto2CsExport_Client.bat
+++ Protobuf/Proto2CsExport_Client.bat
@@ -1,4 +1,4 @@
 cd /d ../Tools/ProtoExport\bin\Debug\net8.0
-start dotnet ProtoExport.dll --mode unity --inputPath ./../../../../../Protobuf --outputPath ./../../../../../Unity/Assets/Hotfix/Proto --namespaceName Hotfix.Proto --isGenerateErrorCode true
+call dotnet ProtoExport.dll --mode unity --inputPath ./../../../../../Protobuf --outputPath ./../../../../../Unity/Assets/Hotfix/Proto --namespaceName Hotfix.Proto --isGenerateErrorCode true
 
 pause
