69c6fd0193e3b01dcd7f62310a1d6f049f60b1be
Blank <wangfj11@foxmail.com>
2026-08-30 15:07:47 +0800

    feat(scripts): 补齐 Godot/LayaBox 生成脚本的 Windows bat 入口
    - Config/gen-godot-json.bat、Protobuf/Proto2CsExport_Godot.bat、Protobuf/Proto2TsExport_LayaBox.bat
    - sh/bat 对应源仓 GameFrameX.Config / GameFrameX.Protobuf 同步提交，明日 aggregate sync 结果一致

---
+++ Protobuf/Proto2TsExport_LayaBox.bat
@@ -0,0 +1,3 @@
+dotnet ./Tools/ProtoExport.dll --mode typescript --inputPath ./ --outputPath ../LayaBox/assets/scripts/protobuf --isGenerateErrorCode true --importPath "../../../src/gameframex/network/"
+
+pause
